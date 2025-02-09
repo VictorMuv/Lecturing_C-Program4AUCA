47ac813bf4139aa3489a92a17a71b29aa75ae677
Victor Muvunyi <muvunyiv555@gmail.com>
2025-02-09 17:48:24 -0500

    Initial commit: Add add_ten.c

---
+++ add_ten.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int number, result;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    result = number + 10;
+    printf("Result after adding 10: %d\n", result);
+    return 0;
+}
